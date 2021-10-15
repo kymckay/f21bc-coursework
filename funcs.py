56b2fd461260d7037c9bf803ad454a586353bb2d
linarietuma <71130806+linarietuma@users.noreply.github.com>
2021-10-15 14:43:28 +0100

    Implement tanh activation func and derivative

--- funcs.py
+++ funcs.py
@@ -13,6 +13,12 @@ def _sigmoid(z):
 def _d_sigmoid(z):
     return _sigmoid(z) * (1 - _sigmoid(z))
 
+def _tanh(z):
+    return (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
+    
+def _d_tanh(z):
+    return 1 - _tanh(z) * _tanh(z)
+
 def _log_loss(y, y_hat):
     # Remember these operations are element-wise
     a = y * np.log(y_hat)
@@ -25,4 +31,5 @@ def _d_log_loss(y, y_hat):
     return (y_hat - y) / (y_hat - y_hat**2)
 
 sigmoid = dfunc(_sigmoid, _d_sigmoid)
+tanh = dfunc(_tanh, _d_tanh)
 log_loss = dfunc(_log_loss, _d_log_loss)
